515de2ad2dae6a8fad2d9629743bc9b33ed23d54
bagasatria <bagasatria.vr.46@gamil.com>
2021-10-01 16:33:25 +0700

    <pustakaboking>

---
+++ pustakaboking/application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 estends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya bagasatria
+        saya tinggal di bogpr hobby tidur";
+    }
+}
+?>
