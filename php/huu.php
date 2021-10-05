86099fcb9f43791e7bbb00839a0331d740953c8c
bagasatria <bagasatria.vr.46@gamil.com>
2021-10-05 15:43:22 +0700

    Menambahkan tugas 2

---
+++ php/huu.php
@@ -0,0 +1,20 @@
+<head>
+<title>Menghitung Volume Kubus</title>
+</head>
+<left>
+ <body bgcolor="white">
+<?php
+
+$teks1 = "Belajar Menghitung";
+$teks2 = " Volume Kubus";
+$hasil = $teks1 . $teks2;
+echo "$hasil";
+
+
+$s=15;
+echo"<table border='0' bgcolor='black' style='color:white'><tr><th colspan='2'<br>
+    Panjang Sisi Kubus = $s<br>\n";
+$volume=$s * $s *$s;
+echo "Volume Kubus = $volume cm3";
+
+?>
---
+++ php/uuu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM KOMENTAR</title>
+</head>
+<body>
+<h1>BUKU TAMU </h1>
+<<br> <br>
+Komentar dan Saran Anda Sangat kami	Butuhkan <br>
+Untuk Meningkatkan Kualitas Situs Kami <br>
+<hr>
+<p> <form action="tampilkomentar.php" method="POST">
+	Nama Anda   	: <input type="text" name="nama" size="30"> <br>
+	Email Anda  	: <input type="text" name="email"> <br>
+	Komentar Anda	: <textarea rows="5" name="Komen"></textarea> <br>
+<input type="submit" value="Kirim"> <input type="reset" value="Batal">
+</form></p>
+</body>
+</html>
